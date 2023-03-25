a24ce75a0225fa1d2b4a99fbe9dd2dd829c90e4e
bahstiien <bastien.lecalvez@gmail.com>
2023-03-25 22:50:24 +0100

    test deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    NEXTAUTH_SECRET:
+      '87ad676f0d33c5607ffb3db96a3b335843359aec173efcbcfb2e1e1dc986aa6e=',
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
